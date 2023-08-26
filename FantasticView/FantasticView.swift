18c21f128823842531ce2da4e6ecba617dc61028
Stefan Adisurya <stefanadisurya@gmail.com>
2023-08-26 10:16:31 +0700

    Add FantasticView

---
+++ FantasticView/FantasticView.swift
@@ -0,0 +1,48 @@
+//
+//  FantasticView.swift
+//  FantasticView
+//
+//  Created by Stefan Adisurya on 26/08/23.
+//
+
+import UIKit
+
+public class FantasticView: UIView {
+    // MARK: - Properties
+    
+    private let colors : [UIColor] = [
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .blue,
+        .purple
+    ]
+    
+    private var colorCounter = 0
+    
+    // MARK: - Life Cycle
+
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        runColorChange()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Functions
+    
+    private func runColorChange() {
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { timer in
+            UIView.animate(withDuration: 2.0) { [weak self] in
+                guard let self = self else { return }
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor
+                self.colorCounter += 1
+            }
+        }
+                
+        scheduledColorChanged.fire()
+    }
+}
---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Stefan Adisurya on 26/08/23.
+//
+
+import UIKit
+
+public final class ViewController: UIViewController {
+    // MARK: - Views
+    
+    private var fantasticView: FantasticView
+    
+    // MARK: - Life Cycle
+    
+    public init(fantasticView: FantasticView = FantasticView(frame: .zero)) {
+        self.fantasticView = fantasticView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fantasticView = FantasticView(frame: .zero)
+        super.init(coder: coder)
+    }
+    
+    override public func viewDidLoad() {
+        let fantasticView = FantasticView(frame: view.bounds)
+        
+        super.viewDidLoad()
+        
+        view.addSubview(fantasticView)
+    }
+}
+
